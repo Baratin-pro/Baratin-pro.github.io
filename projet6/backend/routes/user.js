bedf9aebf36de84f806cd178de58328d7dc95723
Baratin-Pro <baratin.pro@gmail.com>
2020-11-20 09:05:06 +0100

    Restructuration des projets

--- projet6/backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userCtrl = require("../controllers/user.js");
-
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.limiter, userCtrl.login);
-
-module.exports = router;
